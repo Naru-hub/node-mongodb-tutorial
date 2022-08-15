a7c19701fcb2f1b46bf761cd3e506aed4f908adc
Naru-hub <1208ny0324@gmail.com>
2022-08-15 22:44:57 +0900

    MongoDB接続設定

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+// データベース接続
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => console.log("データベース接続に成功しました"))
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("サーバーが起動しました");
